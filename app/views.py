59503ed272d3fbf0b5456bbe835a078e664f94b4
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-01-09 15:21:56 -0500

    Agregar archivos estáticos y templates del proyecto, ejemplo hora actual

--- app/views.py
+++ app/views.py
@@ -1,23 +1,13 @@
 # Create your views here.
 
 from django.http import HttpResponse
-import json
-import urllib2
+from django.template import Context
+from django.template.loader import get_template
+from datetime import datetime
 
-def home(request):
-    try:
-        f = urllib2.urlopen('http://congresorest.appspot.com/diputado/1')
-        result = f.read()
-    except Exception, e:
-        return HttpResponse(e.message)
-    diputado = json.loads(result)
-    return HttpResponse(diputado['entidad'])
-
-def post(request, post_id):
-    if int(post_id) > 10:
-        return HttpResponse('Este post es mayor que 10')
-    else:
-        return HttpResponse('Este post es menor o igual que 10')
-
-def live(request, live_id, curso_id):
-    return HttpResponse('Este es el live %s del curso %s' % (live_id, curso_id))
+def hora_actual(request):
+    now = datetime.now()
+    template = get_template('hora.html')
+    context = Context({ "now": now })
+    html = template.render(context)
+    return HttpResponse(html)
--- puls3/urls.py
+++ puls3/urls.py
@@ -6,9 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'app.views.home', name='home'),
-    url(r'^post/(\d+)$', 'app.views.post', name='post'),
-    url(r'^live/(\d+)/curso/(\d+)$', 'app.views.live', name='live'),
+    url(r'^hora_actual$', 'app.views.hora_actual', name='hora_actual'),
     # url(r'^puls3/', include('puls3.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
