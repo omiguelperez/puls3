365fbba84e7a7a4df6a2cf3b25940d99db8f9d23
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-01-09 23:09:08 -0500

    Reinicializar el proyecto para trabajar el puls3

--- app/views.py
+++ app/views.py
@@ -1,8 +1,1 @@
 # Create your views here.
-
-from django.shortcuts import render_to_response
-from datetime import datetime
-
-def hora_actual(request):
-    now = datetime.now()
-    return render_to_response('hora.html', { 'user': 'omiguelperez', 'now': now })
