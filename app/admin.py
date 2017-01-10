df834bd9e6571bce53978d1252175a637c908685
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-01-10 08:57:34 -0500

    Agregar admin y el index de puls3

---
+++ app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from app.models import Categoria, Enlace
+
+# Register your models here.
+
+admin.site.register(Categoria)
+admin.site.register(Enlace)
--- app/views.py
+++ app/views.py
@@ -1,1 +1,14 @@
 # Create your views here.
+
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django.template.context import Context
+from .models import Categoria, Enlace
+
+def home(request):
+    categorias = Categoria.objects.all()
+    enlaces = Enlace.objects.all()
+    template = get_template('index.html')
+    context = Context({ "categorias": categorias, "enlaces": enlaces })
+    html = template.render(context)
+    return HttpResponse(html)
