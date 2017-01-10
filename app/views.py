f6a498a2539b6760ca4db9ba02698ef26f3ba162
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-01-09 23:07:56 -0500

    Refactor: Ejemplo de hora actual con shortcut render_to_response

--- app/views.py
+++ app/views.py
@@ -1,13 +1,8 @@
 # Create your views here.
 
-from django.http import HttpResponse
-from django.template import Context
-from django.template.loader import get_template
+from django.shortcuts import render_to_response
 from datetime import datetime
 
 def hora_actual(request):
     now = datetime.now()
-    template = get_template('hora.html')
-    context = Context({ "now": now })
-    html = template.render(context)
-    return HttpResponse(html)
+    return render_to_response('hora.html', { 'user': 'omiguelperez', 'now': now })
