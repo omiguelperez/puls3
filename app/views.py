61bd906be3acfd003945bcd5a7f372c1dd4a7f37
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-01-10 11:45:24 -0500

    Agregar login requerido en las views: plus, minus y add

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
 
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template.context import RequestContext
@@ -9,8 +10,9 @@ from .models import Categoria, Enlace
 def home(request):
     categorias = Categoria.objects.all()
     enlaces = Enlace.objects.order_by('-votos').all()
-    return render_to_response('index.html', { 'categorias': categorias, 'enlaces': enlaces })
+    return render_to_response('index.html', { 'request': request, 'categorias': categorias, 'enlaces': enlaces })
 
+@login_required
 def add(request):
     categorias = Categoria.objects.all()
     if request.method == 'POST':
@@ -26,14 +28,16 @@ def categoria(request, categoria_id):
     categorias = Categoria.objects.all()
     categoria = get_object_or_404(Categoria, pk=categoria_id)
     enlaces = Enlace.objects.order_by('-votos').filter(categoria=categoria)
-    return render_to_response('index.html', { 'categorias': categorias, 'enlaces': enlaces })
+    return render_to_response('index.html', { 'request': request, 'categorias': categorias, 'enlaces': enlaces })
 
+@login_required
 def plus(request, enlace_id):
     enlace = Enlace.objects.get(pk=enlace_id)
     enlace.votos = enlace.votos + 1
     enlace.save()
     return HttpResponseRedirect('/')
 
+@login_required
 def minus(request, enlace_id):
     enlace = Enlace.objects.get(pk=enlace_id)
     enlace.votos = enlace.votos - 1
