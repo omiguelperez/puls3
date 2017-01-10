4b2d1f8b597627c95f063c7ce27818b6df86b502
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-01-10 09:06:50 -0500

    Agregar views de 'plus' y 'minus'

--- app/views.py
+++ app/views.py
@@ -1,9 +1,22 @@
 # Create your views here.
 
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from .models import Categoria, Enlace
 
 def home(request):
     categorias = Categoria.objects.all()
-    enlaces = Enlace.objects.all()
+    enlaces = Enlace.objects.order_by('-votos').all()
     return render_to_response('index.html', { "categorias": categorias, "enlaces": enlaces })
+
+def plus(request, enlace_id):
+    enlace = Enlace.objects.get(pk=enlace_id)
+    enlace.votos = enlace.votos + 1
+    enlace.save()
+    return HttpResponseRedirect('/')
+
+def minus(request, enlace_id):
+    enlace = Enlace.objects.get(pk=enlace_id)
+    enlace.votos = enlace.votos - 1
+    enlace.save()
+    return HttpResponseRedirect('/')
