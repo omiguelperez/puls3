4cb2a53c09d377bafe4fc42af2d82d9f62b5df3a
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-01-10 23:10:11 -0500

    Agregar test para probar que un enlace es o no popular

--- app/tests.py
+++ app/tests.py
@@ -5,12 +5,25 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+from django.contrib.auth.models import User
 from django.test import TestCase
+from .models import Categoria, Enlace
 
+class EnlaceTest(TestCase):
+    def test_es_popular(self):
+        categoria = Categoria.objects.create(titulo='Categoria de prueba')
+        usuario = User.objects.create_user(username='miusuario', password='claveusuario')
+        enlace = Enlace.objects.create(titulo='Enlace de prueba',
+            enlace='http://google.com', categoria=categoria, usuario=usuario)
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+        # Probar que el enlace no es popular
+        self.assertEqual(enlace.votos, 0)
+        self.assertEqual(enlace.es_popular(), False)
+        self.assertFalse(enlace.es_popular())
+
+        # Probar que el enlace es popular
+        enlace.votos = 20
+        enlace.save()
+        self.assertEqual(enlace.votos, 20)
+        self.assertEqual(enlace.es_popular(), True)
+        self.assertTrue(enlace.es_popular())
