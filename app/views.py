13699bbc6d8bd5af9863dd95e54bc2cfdb4b40e6
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-01-10 21:17:17 -0500

    Refactor: Cambiar de shortcut render_to_response to render

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@
 
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render, render_to_response, get_object_or_404
 from django.template.context import RequestContext
 from .forms import EnlaceForm
 from .models import Categoria, Enlace
@@ -10,7 +10,7 @@ from .models import Categoria, Enlace
 def home(request):
     categorias = Categoria.objects.all()
     enlaces = Enlace.objects.order_by('-votos').all()
-    return render_to_response('index.html', { 'request': request, 'categorias': categorias, 'enlaces': enlaces })
+    return render(request, 'index.html', { 'request': request, 'categorias': categorias, 'enlaces': enlaces })
 
 @login_required
 def add(request):
@@ -24,13 +24,13 @@ def add(request):
             return HttpResponseRedirect('/')
     else:
         form = EnlaceForm()
-    return render_to_response('form.html', context_instance=RequestContext(request, { 'form': form, 'categorias': categorias }))
+    return render(request, 'form.html', { 'form': form, 'categorias': categorias })
 
 def categoria(request, categoria_id):
     categorias = Categoria.objects.all()
     categoria = get_object_or_404(Categoria, pk=categoria_id)
     enlaces = Enlace.objects.order_by('-votos').filter(categoria=categoria)
-    return render_to_response('index.html', { 'request': request, 'categorias': categorias, 'enlaces': enlaces })
+    return render(request, 'index.html', { 'request': request, 'categorias': categorias, 'enlaces': enlaces })
 
 @login_required
 def plus(request, enlace_id):
