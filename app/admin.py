ca0b129087634c806be0071d32317030ccf5b40c
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-01-22 20:42:11 -0500

    Agregar modelos de pulse al administrador

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Agregador, Categoria, Enlace
 
 # Register your models here.
+
+class EnlaceInline(admin.StackedInline):
+    model = Enlace
+    extra = 2
+    raw_id_fields = ('usuario',)
+
+class EnlaceAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'enlace', 'categoria', 'imagen_voto', 'es_popular',)
+    list_filter = ('categoria', 'usuario',)
+    search_fields = ('categoria__titulo', 'usuario__email',)
+    list_editable = ('titulo', 'enlace', 'categoria',)
+    list_display_links = ('es_popular', 'imagen_voto',)
+    raw_id_fields = ('categoria', 'usuario',)
+
+    def imagen_voto(self, enlace):
+        url = enlace.mis_votos_en_imagen_rosada()
+        tag = '<img src="%s">' % url
+        return tag
+    imagen_voto.allow_tags = True
+    imagen_voto.admin_order_field = 'votos'
+
+class CategoriaAdmin(admin.ModelAdmin):
+    inlines = (EnlaceInline,)
+
+class AgregadorAdmin(admin.ModelAdmin):
+    filter_horizontal = ['enlaces']
+
+admin.site.register(Agregador, AgregadorAdmin)
+admin.site.register(Categoria, CategoriaAdmin)
+admin.site.register(Enlace, EnlaceAdmin)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    titulo = models.CharField(max_length=140)
+
+    def __str__(self):
+        return self.titulo
+
+class Enlace(models.Model):
+    titulo = models.CharField(max_length=140)
+    enlace = models.URLField()
+    votos = models.IntegerField(default=0)
+    categoria = models.ForeignKey(Categoria)
+    usuario = models.ForeignKey(User)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.titulo, self.enlace)
+
+    def mis_votos_en_imagen_rosada(self):
+        return 'http://placehold.it/250x150/E8117F/FFFFFF?text=%s+votos' % self.votos
+
+    def es_popular(self):
+        return self.votos > 10
+    es_popular.boolean = True
+
+class Agregador(models.Model):
+    titulo = models.CharField(max_length=140)
+    enlaces = models.ManyToManyField(Enlace)
+
+    class Meta:
+        verbose_name_plural = 'Agregadores'
+
+    def __str__(self):
+        return self.titulo
